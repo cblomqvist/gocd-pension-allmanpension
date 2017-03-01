aeff2a269d3b291ac9c1250336222a6cd2cb596e
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-03-01 11:39:55 +0100

    Fördelning 86,5%/13,5%

--- calc_inkomstpension.sh
+++ calc_inkomstpension.sh
@@ -1,3 +1,5 @@
+source fordelning.sh
 inkomst=$(cat target/pensionsratter.txt)
-res=$(echo "scale=0; (${inkomst} * 0.135) / 1" | bc -l)
+res=$(echo "scale=0; (${inkomst} * ${INKOMSTPENSION}) / 1" | bc -l)
 echo ${res} > target/inkomstpension.txt
+
---
+++ calc_premiepension.sh
@@ -0,0 +1,4 @@
+source fordelning.sh
+inkomst=$(cat target/pensionsratter.txt)
+res=$(echo "scale=0; (${inkomst} * ${PREMIEPENSION}) / 1" | bc -l)
+echo ${res} > target/premiepension.txt
