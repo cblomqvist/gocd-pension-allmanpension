3a5f9973ed6a7d09685445e1a06eff9175a5918d
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-03-02 15:16:02 +0100

    Use specific output directories and generic file names

--- calc_inkomstpension.sh
+++ calc_inkomstpension.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
-mkdir -p output
+mkdir -p output/inkomstpension
 source ./fordelning.sh
 inkomst=$(cat input/pensionsratter/res.txt)
 res=$(echo "scale=0; (${inkomst} * ${INKOMSTPENSION}) / 1" | bc -l)
-echo ${res} > output/inkomstpension.txt
+echo ${res} > output/inkomstpension/res.txt
--- calc_premiepension.sh
+++ calc_premiepension.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
-mkdir -p output
+mkdir -p output/premiepension
 source ./fordelning.sh
 inkomst=$(cat input/pensionsratter/res.txt)
 res=$(echo "scale=0; (${inkomst} * ${PREMIEPENSION}) / 1" | bc -l)
-echo ${res} > output/premiepension.txt
+echo ${res} > output/premiepension/res.txt
