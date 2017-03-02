a618314428659dc43a292e36c17fdce07cc4d847
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-03-02 15:05:11 +0100

    Use generic input file name res.txt in expected subfolder

--- calc_inkomstpension.sh
+++ calc_inkomstpension.sh
@@ -2,6 +2,6 @@
 set -e
 mkdir -p output
 source ./fordelning.sh
-inkomst=$(cat input/pensionsratter.txt)
+inkomst=$(cat input/pensionsratter/res.txt)
 res=$(echo "scale=0; (${inkomst} * ${INKOMSTPENSION}) / 1" | bc -l)
 echo ${res} > output/inkomstpension.txt
